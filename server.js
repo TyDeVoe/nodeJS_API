33be590bc6ac87dde5ea02292940ba96e5a4837f
TyDeVoe <taylorb.devoe@gmail.com>
2022-01-28 13:52:48 -0700

    update

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const port = 3000;
 const app = express();
+const md5 = require('md5')
 
 app.get('/',(req,res)=>{
     res.send("Hello browser");
@@ -12,7 +13,8 @@ app.use(bodyParser.json());
 
 app.post('/login', (req,res) =>{
     console.log(JSON.stringify(req.body));
-    if (req.body.userName =="tydevoe" && req.body.password=="51bca78ac0e26e237d068ed646504c36"){
+    let passHash = md5(req.body.password);
+    if (req.body.userName =="tydevoe" && passHash=="51bca78ac0e26e237d068ed646504c36"){
         res.send("Welcome!")
     } else{
         res.send("Get thee hence, Hacker!");
