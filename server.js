5972955637a507eb65568d05d7d222ee673fb318
TyDeVoe <taylorb.devoe@gmail.com>
2022-01-28 13:37:44 -0700

    update

--- server.js
+++ server.js
@@ -1,2 +1,23 @@
-import sayHello from '../'
-console.log('yo')
+const express = require('express');
+const bodyParser = require('body-parser')
+const port = 3000;
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send("Hello browser");
+});
+app.use(bodyParser.json());
+
+
+
+app.post('/login', (req,res) =>{
+    console.log(JSON.stringify(req.body));
+    if (req.body.userName =="tydevoe" && req.body.password=="51bca78ac0e26e237d068ed646504c36"){
+        res.send("Welcome!")
+    } else{
+        res.send("Get thee hence, Hacker!");
+    }
+
+});
+
+app.listen(port, ()=>{});
