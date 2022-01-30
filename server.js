e7ede35db3ad096b597399c440f260f9c8d4a20c
TyDeVoe <taylorb.devoe@gmail.com>
2022-01-29 18:52:33 -0700

    hashUpdate

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ app.use(bodyParser.json());
 app.post('/login', (req,res) =>{
     console.log(JSON.stringify(req.body));
     let passHash = md5(req.body.password);
-    if (req.body.userName =="tydevoe" && passHash=="51bca78ac0e26e237d068ed646504c36"){
+    let userHash = md5(req.body.userName);
+    if (userHash =="26367e3e79c46bd3296560b584f1a858" && passHash=="51bca78ac0e26e237d068ed646504c36"){
         res.send("Welcome!")
     } else{
         res.send("Get thee hence, Hacker!");
